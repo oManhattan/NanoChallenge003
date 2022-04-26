a0dc7ee7db835a7f0bf9581780df5406dcae2936
oManhattan <matheusbatuque@gmail.com>
2022-04-25 23:42:40 -0300

    update

---
+++ CoreDataTest/CoreDataTest/Utilities/UITextView+Additions.swift
@@ -0,0 +1,24 @@
+//
+//  UITextView+Additions.swift
+//  CoreDataTest
+//
+//  Created by Matheus Cavalcanti de Arruda on 19/04/22.
+//
+
+import Foundation
+import UIKit
+
+extension UITextView {
+    
+    func addDoneButton(title: String, target: Any, selector: Selector) {
+        
+        let toolBar = UIToolbar(frame: CGRect(x: 0.0,
+                                              y: 0.0,
+                                              width: UIScreen.main.bounds.size.width,
+                                              height: 44.0))//1
+        let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)//2
+        let barButton = UIBarButtonItem(title: title, style: .done, target: target, action: selector)//3
+        toolBar.setItems([flexible, barButton], animated: false)//4
+        self.inputAccessoryView = toolBar//5
+    }
+}
