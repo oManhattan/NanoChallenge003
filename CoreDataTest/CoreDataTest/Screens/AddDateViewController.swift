53934814e1c28d6e8dc2aa230512e03f71d714e3
oManhattan <matheusbatuque@gmail.com>
2022-04-25 23:31:39 -0300

    update

--- CoreDataTest/CoreDataTest/Screens/AddDateViewController.swift
+++ CoreDataTest/CoreDataTest/Screens/AddDateViewController.swift
@@ -9,42 +9,30 @@ import UIKit
 
 class AddDateViewController: UIViewController {
     
-    var scrollView = UIScrollView()
+    private weak var scrollView: UIScrollView?
+    private weak var navBar: UINavigationBar?
+    private weak var navItem: UINavigationItem?
+    private weak var cancelButton: UIBarButtonItem?
+    private weak var saveButton: UIBarButtonItem?
+    private weak var statusSelector: RadioButtonGroup?
+    private weak var datePicker: UIDatePicker?
+    private weak var textView: UITextView?
     
-    var navBar = UINavigationBar()
-    var navItem = UINavigationItem()
-    var cancelButton = UIBarButtonItem()
-    var saveButton = UIBarButtonItem()
-    
-    var datePicker = UIDatePicker()
-    
-    var tableView = UITableView()
-    let customCell = RadioButtonTableViewCell()
-    
-    var textView = UITextView()
-    
-    private var options = [radio]()
-
-    struct radio {
-        var title: String
-        var uncheckedImage: String
-        var checkedImage: String
-        var currentImage: UIImage
-        
-        var checked: Bool {
-            didSet {
-                currentImage = (checked ? UIImage(named: checkedImage) : UIImage(named: uncheckedImage))!
-            }
-        }
-    }
-
+    public var selectedStatus: Int = 3
+    public var selectedDate: Date = Date.now
+    public var notes: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        options.append(radio(title: "Não entendi", uncheckedImage: "RedStatus.png", checkedImage: "SelectedRed.png", currentImage: UIImage(named: "RedStatus.png")!, checked: false))
-        options.append(radio(title: "Estudar mais", uncheckedImage: "YellowStatus.png", checkedImage: "SelectedYellow.png", currentImage: UIImage(named: "YellowStatus.png")!, checked: false))
-        options.append(radio(title: "Entendi", uncheckedImage: "GreenStatus.png", checkedImage: "SelectedGreen.png", currentImage: UIImage(named: "GreenStatus")!, checked: false))
+        let navBar = UINavigationBar()
+        let navItem = UINavigationItem()
+        var cancelButton = UIBarButtonItem()
+        var saveButton = UIBarButtonItem()
+        let scrollView = UIScrollView()
+        let datePicker = UIDatePicker()
+        let statusSelector = RadioButtonGroup()
+        let textView = UITextView()
         
         view.addSubview(navBar)
         view.addSubview(scrollView)
@@ -63,21 +51,17 @@ class AddDateViewController: UIViewController {
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         scrollView.contentSize = CGSize(width: view.bounds.width, height: view.frame.height)
         scrollView.addSubview(datePicker)
-        scrollView.addSubview(tableView)
+        scrollView.addSubview(statusSelector)
         scrollView.addSubview(textView)
         
         datePicker.translatesAutoresizingMaskIntoConstraints = false
         datePicker.preferredDatePickerStyle = .inline
         datePicker.maximumDate = Date.now
         datePicker.locale = Locale(identifier: "pt_BR")
+        datePicker.timeZone = TimeZone(identifier: "America/Sao_Paulo")
         datePicker.datePickerMode = .date
         
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.register(customCell.nib(), forCellReuseIdentifier: customCell.identifier)
-        tableView.isScrollEnabled = false
-        tableView.separatorStyle = .none
-        tableView.delegate = self
-        tableView.dataSource = self
+        statusSelector.translatesAutoresizingMaskIntoConstraints = false
         
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.layer.borderWidth = 1
@@ -105,102 +89,88 @@ class AddDateViewController: UIViewController {
             datePicker.leftAnchor.constraint(equalTo: view.leftAnchor),
             datePicker.rightAnchor.constraint(equalTo: view.rightAnchor),
 
-            tableView.topAnchor.constraint(equalTo: datePicker.bottomAnchor),
-            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
-            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
-            tableView.heightAnchor.constraint(equalToConstant: 165),
+            statusSelector.topAnchor.constraint(equalTo: datePicker.bottomAnchor),
+            statusSelector.leftAnchor.constraint(equalTo: view.leftAnchor),
+            statusSelector.rightAnchor.constraint(equalTo: view.rightAnchor),
+            statusSelector.heightAnchor.constraint(equalToConstant: 165),
 
-            textView.topAnchor.constraint(equalTo: tableView.bottomAnchor, constant: 15),
+            textView.topAnchor.constraint(equalTo: statusSelector.bottomAnchor, constant: 15),
             textView.leftAnchor.constraint(equalTo: view.leftAnchor),
             textView.rightAnchor.constraint(equalTo: view.rightAnchor),
             textView.heightAnchor.constraint(equalToConstant: 300)
         ])
+        
+        self.navBar = navBar
+        self.navItem = navItem
+        self.cancelButton = cancelButton
+        self.saveButton = saveButton
+        self.scrollView = scrollView
+        self.datePicker = datePicker
+        self.statusSelector = statusSelector
+        self.textView = textView
+        
+        self.datePicker?.addTarget(self, action: #selector(didSelectDate(_:)), for: .valueChanged)
+        self.statusSelector?.addTarget(self, action: #selector(didSelectStatus(_:)), for: .valueChanged)
         
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-    }
-    
-    @objc func cancelFunc() {
+    @objc private func cancelFunc() {
         dismiss(animated: true)
     }
     
-    @objc func saveFunc() {
+    @objc private func saveFunc() {
+        
+        guard selectedStatus < 3 else {
+            
+            let alertScreen = UIAlertController(title: "Status não selecionado", message: "Selecione um status.", preferredStyle: .alert)
+            let okayBtn = UIAlertAction(title: "OK", style: .cancel)
+            alertScreen.addAction(okayBtn)
+            
+            present(alertScreen, animated: true)
+            
+            return
+        }
+        
         dismiss(animated: true)
     }
     
-    @objc func adjustForKeyboard(notification: Notification) {
+    @objc private func didSelectDate(_ sender: UIDatePicker) {
+        if sender.date >= Date.now {
+            sender.date = Date.now
+        }
+        
+        selectedDate = sender.date
+    }
+    
+    @objc private func didSelectStatus(_ sender: RadioButtonGroup) {
+        selectedStatus = sender.selectedStatus
+    }
+    
+    @objc private func adjustForKeyboard(notification: Notification) {
         guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
 
         let keyboardScreenEndFrame = keyboardValue.cgRectValue
         let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
 
         if notification.name == UIResponder.keyboardWillHideNotification {
-            scrollView.contentSize = CGSize(width: view.bounds.width, height: view.frame.height + 50)
+            scrollView?.contentSize = CGSize(width: view.bounds.width, height: view.frame.height + 50)
         } else {
-            scrollView.contentSize = CGSize(width: view.bounds.width, height: view.bounds.height + keyboardViewEndFrame.height + 44)
-            scrollView.contentOffset.y = textView.frame.maxY - keyboardViewEndFrame.height + 44
+            scrollView?.contentSize = CGSize(width: view.bounds.width, height: view.bounds.height + keyboardViewEndFrame.height + 44)
+            scrollView?.contentOffset.y = (textView?.frame.maxY)! - keyboardViewEndFrame.height + 44
         }
 
-        textView.scrollIndicatorInsets = textView.contentInset
+        textView?.scrollIndicatorInsets = textView!.contentInset
 
-        let selectedRange = textView.selectedRange
-        textView.scrollRangeToVisible(selectedRange)
+        let selectedRange = textView?.selectedRange
+        textView?.scrollRangeToVisible(selectedRange!)
     }
     
     @objc func tapDone(sender: Any) {
             self.view.endEditing(true)
         }
-}
-
-extension AddDateViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 55
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        switch indexPath.row {
-        case 0:
-            options[0].checked = true
-            options[1].checked = false
-            options[2].checked = false
-        case 1:
-            options[0].checked = false
-            options[1].checked = true
-            options[2].checked = false
-        case 2:
-            options[0].checked = false
-            options[1].checked = false
-            options[2].checked = true
-        default:
-            options[0].checked = false
-            options[1].checked = false
-            options[2].checked = false
-        }
-        
-        tableView.reloadData()
-        
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-}
-
-extension AddDateViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return options.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: customCell.identifier, for: indexPath) as! RadioButtonTableViewCell
-        
-        cell.title.text = options[indexPath.row].title
-        cell.statusImage.image = options[indexPath.row].currentImage
-        
-        return cell
-    }
 }
 
 extension AddDateViewController: UITextViewDelegate {
